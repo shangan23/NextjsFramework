7a3021a58f78396b4470790a9fb7c29ce54fe021
Shankar Ganesh Jayaram <shankar@coremobileinc.com>
2020-07-06 23:22:13 +0530

    feat(base):order added, wizard in progress

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  API: 'http://localhost:3001/api',
-  IMGPath: 'http://localhost:3001/api/images/',
+  API: 'http://192.168.1.3:3001/api',
+  IMGPath: 'http://192.168.1.3:3001/api/images/',
   MSG: {
     'authError': 'Authentication Failed.'
   },

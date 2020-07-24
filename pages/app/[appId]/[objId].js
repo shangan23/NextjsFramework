57039abbd0067e7107a8f1a9a6e2b0e211022e95
Shankar Ganesh Jayaraman <shangan.23@gmail.com>
2020-07-24 18:54:52 +0530

    feat(base): working on popover feature

--- pages/app/[appId]/[objId].js
+++ pages/app/[appId]/[objId].js
@@ -18,7 +18,6 @@ import Step from '@material-ui/core/Step';
 import StepButton from '@material-ui/core/StepButton';
 import Router from 'next/router';
 import Chip from '@material-ui/core/Chip';
-import Avatar from '@material-ui/core/Avatar';
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
 import DynamicSetTable from '../../../components/DynamicSetTable';
 
@@ -31,46 +30,28 @@ import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import BuildIcon from '@material-ui/icons/Build';
 
-const iconDisplay = (module) => {
-  let iconMeta = modules(module).icon;
-  //console.log
-  let icon;
-  switch (iconMeta.name) {
-    case 'AccountCircleIcon':
-      icon = <AccountCircleIcon />
-      break;
-    case 'RecentActorsIcon':
-      icon = <RecentActorsIcon />
-      break;
-    case 'ContactsIcon':
-      icon = <ContactsIcon />
-      break;
-    case 'AppsIcon':
-      icon = <AppsIcon />
-      break;
-    case 'AssignmentIcon':
-      icon = <AssignmentIcon />
-      break;
-    case 'ShoppingCartIcon':
-      icon = <ShoppingCartIcon />
-      break;
-    case 'PlaylistAddCheckIcon':
-      icon = <PlaylistAddCheckIcon />
-      break;
-    case 'BuildIcon':
-      icon = <BuildIcon />
-      break;
-  }
-  return icon;
-};
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+import Popover from '@material-ui/core/Popover';
 
 const useStyles = theme => ({
   root: {
     width: '100%',
   },
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
   appBar: {
     top: theme.spacing(0),
-    //marginLeft: theme.spacing(-2),
     position: 'relative'
   },
   toolbarStyle: {
@@ -114,8 +95,12 @@ const useStyles = theme => ({
   },
   TableCellHead: {
     fontSize: '0.85rem',
-    fontWeight: 500,
+    color: theme.palette.primary.light,
+    fontWeight: 400,
     padding: theme.spacing(1)
+  },
+  chipIconBg: {
+    color: theme.palette.primary.main
   }
 });
 
@@ -144,7 +129,7 @@ const frameURL = async (req) => {
 class DynamicCreate extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { activeStep: 0 };
+    this.state = { activeStep: 0, anchorEl: null, currentPopModule: null };
   }
 
   static async getInitialProps(ctx) {
@@ -165,9 +150,23 @@ class DynamicCreate extends React.Component {
 
   render() {
 
-    let module = this.props.module;
-    let moduleMeta = modules(module);
-    let steps = [], subApp = [];
+    let module = this.props.module,
+      moduleMeta = modules(module),
+      fieldsToRender = moduleController(module, this.props.siteInfo),
+      object = this.props.objJson,
+      objTitle, objCreatedBy, objSubTitle, steps = [], subApp = [];
+
+    const { classes } = this.props,
+      open = Boolean(this.state.anchorEl);
+
+    const handlePopoverOpen = (event, module) => {
+      console.log('event.currentTarget',event.currentTarget)
+      this.setState({ anchorEl: event.currentTarget, currentPopModule: module });
+    };
+
+    const handlePopoverClose = () => {
+      this.setState({ anchorEl: null });
+    };
 
     if (moduleMeta) {
       subApp = moduleMeta.subApp;
@@ -177,13 +176,38 @@ class DynamicCreate extends React.Component {
       }
     }
 
-    const { classes } = this.props;
+    const iconDisplay = (module) => {
+      let iconMeta = modules(module).icon;
+      let icon;
+      switch (iconMeta.name) {
+        case 'AccountCircleIcon':
+          icon = <AccountCircleIcon className={classes.chipIconBg} />
+          break;
+        case 'RecentActorsIcon':
+          icon = <RecentActorsIcon className={classes.chipIconBg} />
+          break;
+        case 'ContactsIcon':
+          icon = <ContactsIcon className={classes.chipIconBg} />
+          break;
+        case 'AppsIcon':
+          icon = <AppsIcon className={classes.chipIconBg} />
+          break;
+        case 'AssignmentIcon':
+          icon = <AssignmentIcon className={classes.chipIconBg} />
+          break;
+        case 'ShoppingCartIcon':
+          icon = <ShoppingCartIcon className={classes.chipIconBg} />
+          break;
+        case 'PlaylistAddCheckIcon':
+          icon = <PlaylistAddCheckIcon className={classes.chipIconBg} />
+          break;
+        case 'BuildIcon':
+          icon = <BuildIcon className={classes.chipIconBg} />
+          break;
+      }
+      return icon;
+    };
 
-    let fieldsToRender = moduleController(module, this.props.siteInfo);
-    let object = this.props.objJson;
-
-    console.log(object);
-    let objTitle, objCreatedBy, objSubTitle;
     fieldsToRender.filter(function (obj) {
       if (obj.primary) {
         objTitle = object[obj.id];
@@ -200,7 +224,7 @@ class DynamicCreate extends React.Component {
       }
 
       if (obj.fk && obj.id === 'fk_createdBy') {
-        objCreatedBy = object['fk_createdBy'].fullName;
+        objCreatedBy = object[obj.id][obj.moduleField];
       }
     });
 
@@ -211,7 +235,102 @@ class DynamicCreate extends React.Component {
       );
     };
 
-    fieldsToRender.sort(dynamicSort('section'));
+    fieldsToRender.sort(dynamicSort('section')); // Gropu/rearrange fields by Sections
+
+    const tabelPopOver = (
+      <Paper className={classes.root} elevation={0} variant="outlined">
+        <TableContainer className={classes.container}>
+          <Table stickyHeader size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  className={classes.TableCellHead}
+                  key={Math.random()}
+                  align={'center'}
+                >
+                  Item
+                </TableCell>
+                <TableCell
+                  className={classes.TableCellHead}
+                  key={Math.random()}
+                  align={'center'}
+                >
+                  Item Code
+                </TableCell>
+                <TableCell
+                  className={classes.TableCellHead}
+                  key={Math.random()}
+                  align={'center'}
+                >
+                  On Available
+                </TableCell>
+                <TableCell
+                  className={classes.TableCellHead}
+                  key={Math.random()}
+                  align={'center'}
+                >
+                  On Order
+                </TableCell>
+                <TableCell
+                  className={classes.TableCellHead}
+                  key={Math.random()}
+                  align={'center'}
+                >
+                  On Hand
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow hover role="checkbox" tabIndex={-1} key={Math.random()} >
+                <TableCell className={classes.TableCell} key={Math.random()} align={'center'}>
+                  Bike
+                </TableCell>
+                <TableCell className={classes.TableCell} key={Math.random()} align={'center'}>
+                  B123
+                </TableCell>
+                <TableCell className={classes.TableCell} key={Math.random()} align={'center'}>
+                  0
+                </TableCell>
+                <TableCell className={classes.TableCell} key={Math.random()} align={'center'}>
+                  0
+                </TableCell>
+                <TableCell className={classes.TableCell} key={Math.random()} align={'center'}>
+                  20
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    );
+
+    const popOver = (id) => {
+      return (<Popover
+        id={id}
+        className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+        open={open}
+        anchorEl={this.state.anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography variant="overline" color="secondary"> Inventory - {this.state.currentPopModule}</Typography>
+       {tabelPopOver}
+      {/*  <Typography variant="overline" color="secondary"> Inventory - {this.state.currentPopModule}</Typography>
+      {tabelPopOver} */}
+      </Popover>)
+    };
+
     const renderFields = (
       <Grid container spacing={2} className={classes.fields} key={`grid-form${Math.random()}`}>
         {
@@ -249,17 +368,22 @@ class DynamicCreate extends React.Component {
                   || (fieldsToRender[index]['type'] == 'Lookup') &&
                   <Grid item xs={12} md={4} key={index}>
                     <Typography variant="caption">{fieldsToRender[index]['label']}:</Typography>
-                    <Typography variant="body2">
+                    <Typography
+                      aria-owns={open ? `mouse-over-popover${index}` : undefined}
+                      aria-haspopup="true"
+                      variant="body2"
+                      onMouseEnter={(e) => handlePopoverOpen(e, fieldsToRender[index]['module'])}
+                      onMouseLeave={handlePopoverClose}
+                    >
                       <Chip
                         icon={iconDisplay(fieldsToRender[index]['module'])}
-                        //variant=""
-                        //avatar={<Avatar className={classes.purple}>{fieldsToRender[index]['module'].slice(0, 1).toUpperCase()}</Avatar>}
                         size="small"
                         label={`${object[fieldsToRender[index]['id']][fieldsToRender[index]['moduleField']]}`}
                         onClick={() => Router.push('/app/[appId]/[objId]', `/app/${fieldsToRender[index]['module']}/${object[fieldsToRender[index]['id']].id}`)}
                         clickable
                       />
                     </Typography>
+                    {popOver(`mouse-over-popover${index}`)}
                   </Grid>
                   || (fieldsToRender[index]['id'] != 'action') &&
                   <Grid item xs={12} md={4} key={index}>
@@ -273,19 +397,16 @@ class DynamicCreate extends React.Component {
       </Grid>
     );
 
-    const stepRender = (<div className={classes.root}>
+    const stepRender = (
       <div>
-        <div>
-          {renderFields}
-        </div>
-      </div>
-    </div>);
+        {renderFields}
+      </div>);
 
     return (
       <Grid container spacing={0} key={`${Math.random()}`}>
         <Grid item xs={12} md={12} key={`${Math.random()}`}>
-          <AppBar key={`${Math.random()}`} elevation={0}  color="transparent" position="fixed" className={classes.appBar}>
-            <Toolbar  key={`${Math.random()}`} className={classes.toolbarStyle} variant="dense">
+          <AppBar key={`${Math.random()}`} elevation={0} color="transparent" position="fixed" className={classes.appBar}>
+            <Toolbar key={`${Math.random()}`} className={classes.toolbarStyle} variant="dense">
               <div key={`${Math.random()}`}>
                 <Typography color="primary" variant="h6">{objTitle} ({objSubTitle})</Typography>
                 <Typography color="textSecondary" variant="caption">
